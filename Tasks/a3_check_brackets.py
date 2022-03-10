c9193516637cdf5df78e0c7fc7b444b1b282333b
kirillvoroncov <kir-v@bk.ru>
2022-03-10 16:12:42 +0300

    a3

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -1,9 +1,17 @@
 def check_brackets(brackets_row: str) -> bool:
+
     """
     Check whether input string is a valid bracket sequence
     Valid examples: "", "()", "()()(()())", invalid: "(", ")", ")("
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    left_ = brackets_row.count('(')
+    right_ = brackets_row.count(')')
+    if left_ == right_ and brackets_row.startswith(')') == False or left_ == 0 and right_ == 0:
+        return True
+    else:
+        return False
 
-    return False
+
+
