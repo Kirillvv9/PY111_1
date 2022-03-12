66b679dd3fd753f4c0c3be6fd5934a4f117593b1
kirillvoroncov <kir-v@bk.ru>
2022-03-12 11:36:35 +0300

    c0

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -7,6 +7,8 @@ def fib_recursive(n: int) -> int:
     """
     if n in (1, 2):
         return 1
+    if n < 0:
+        raise ValueError
     return fib_recursive(n - 1) + fib_recursive(n - 2)
 
 
@@ -17,7 +19,10 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
+    if n < 0:
+        raise ValueError
     fib1 = fib2 = 1
     for i in range(2, n):
         fib1, fib2 = fib2, fib1 + fib2
     return fib2
+
