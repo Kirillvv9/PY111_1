3bbaf40aa933b462ee5fe2251db161fbd7bb6eea
kirillvoroncov <kir-v@bk.ru>
2022-03-17 09:43:01 +0300

    d0

---
+++ Tasks/d0_stairway.py
@@ -0,0 +1,16 @@
+from typing import Union, Sequence
+
+
+def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    """
+    Calculate min cost of getting to the top of stairway if agent can go on next or through one step.
+
+    :param stairway: list of ints, where each int is a cost of appropriate step
+    :return: minimal cost of getting to the top
+    """
+
+    stairs = len(stairway)  # считаем количесвто ступеней
+    for i in range(2, stairs):
+        stairway[i] = stairway[i] + min(stairway[i - 1], stairway[i - 2])  # заменяем цену каждой ступени на
+        # минимальную стоимость подхода к ней ([1, 10, 5, 11] -> [1, 10, 6 (5 + 1), 11]...)
+    return stairway[-1]  # получился новый список, где последний элемент - минимальная стоимость прохода по лестнице
