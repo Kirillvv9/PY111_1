532eea6eebbd3e5063e9a966e505f0c309a626e9
kirillvoroncov <kir-v@bk.ru>
2022-03-17 09:45:49 +0300

    b2

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,6 +2,9 @@
 Taylor series
 """
 from typing import Union
+import math
+from itertools import count
+constant = 0.000000001
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,8 +14,14 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    total = 0
+    n = 0
+    while True:
+        f_ex = (x ** n) / (math.factorial(n))
+        n += 1
+        total += f_ex
+        if f_ex < constant:
+            return total
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +31,11 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    total = 0
+
+    for n in count(0, 1):
+        f_sin = ((-1) ** n) * ((x ** (2 * n + 1)) / (math.factorial(2 * n + 1)))
+        total += f_sin
+        if abs(f_sin) <= constant:
+            return total
+
