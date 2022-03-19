7df9d67fecf50a9608c97b5e0a3a377391bb60cf
kirillvoroncov <kir-v@bk.ru>
2022-03-19 14:37:42 +0300

    d1

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -6,5 +6,23 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    print(shape, point)
-    return 0
+    rows, cols = shape[0], shape[1]
+    row_, col_ = point[0], point[1]
+
+    board = [[0 for i in range(cols)] for j in range(rows)]
+    board[0][0] = 1
+
+    for r in range(rows):
+        for c in range(cols):
+            if r - 1 >= 0 and c + 2 < cols:
+                board[r][c] += 2 * board[r - 1][c + 2]
+            if r - 1 >= 0 and c - 2 >= 0:
+                board[r][c] += 2 * board[r - 1][c - 2]
+            if r - 2 >= 0 and c - 1 >= 0:
+                board[r][c] += 2 * board[r - 2][c - 1]
+            if r - 2 >= 0 and c + 1 < cols:
+                board[r][c] += 2 * board[r - 2][c + 1]
+
+    return board[row_][col_]
+
+
